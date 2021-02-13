01b3914929558427ac66f6fa549d34a2b137d22e
sunxiangyu <1340518678@qq.com>
2021-02-13 16:20:15 +0800

    更新

--- learndjango/projects/views.py
+++ learndjango/projects/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse, Http404
 from django.views import View
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework import mixins
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -13,85 +14,39 @@ from projects.serializer import ProjectModelSerializer
 from utils.pagination import PageNumberPaginationManual
 
 
-class ProjectsList(GenericAPIView):
+class ProjectsList(GenericAPIView, mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin, ):
     queryset = Projects.objects.all()
     serializer_class = ProjectModelSerializer
 
-    # 排序
-    # filter_backends = [filters.OrderingFilter]
-    # ordering_fields = ['name', 'leader']
-
-    # 过滤
-    # filter_backends = [DjangoFilterBackend]
-    # filterset_fields = ['name', 'leader']
     pagination_class = PageNumberPaginationManual
 
-    def get(self, request):
-        # project = Projects.objects.all()
-        # serializer = ProjectModelSerializer(instance=project, many=True)
-        project_qs = self.get_queryset()
-        project_qs = self.filter_queryset(project_qs)
-        page = self.paginate_queryset(project_qs)
-        if page is not None:
-            serializer = self.get_serializer(instance=page, many=True)
-            self.get_paginated_response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-        return Response(serializer.data, status=201)
-
-    def post(self, request):
-        json_data = request.body.decode('utf-8')
-        py_data = json.loads(json_data, encoding='utf-8')
-        serializer = ProjectModelSerializer(data=py_data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except Exception as e:
-            return JsonResponse(serializer.errors)
-        serializer.save()
-        # project = Projects.objects.create(**serializer.validated_data)
-        return JsonResponse(serializer.data, status=201)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ProjectsDetail(GenericAPIView):
+class ProjectsDetail(GenericAPIView,
+                     mixins.CreateModelMixin,
+                     mixins.RetrieveModelMixin,
+                     mixins.DestroyModelMixin,
+                     mixins.UpdateModelMixin):
     queryset = Projects.objects.all()
     serializer_class = ProjectModelSerializer
-    # filter_backends = [filters.OrderingFilter]
-    # ordering_fields = ['name', 'leader']
-    # filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'leader']
 
     # lookup_field = 'id'
-    def get(self, request, pk):
-        project = self.get_object()
-        serializer = self.get_serializer(instance=project)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk):
-        project = self.get_object()
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-        json_data = request.body.decode('utf-8')
-        py_data = json.loads(json_data, encoding='utf-8')
-        serializer = self.get_serializer(instance=project, data=py_data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except Exception as e:
-            return JsonResponse(serializer.errors)
-        serializer.save()
-        # project = Projects.objects.create(**serializer.validated_data)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
-        return JsonResponse(serializer.data, status=201)
-
-    def delete(self, request, pk):
-        project = self.get_object()
-        project.delete()
-        return Response(None, status=204)
-
-    def post(self, request):
-        data = request.body.decode('utf-8')
-        py_date = json.loads(data)
-        serializer = ProjectModelSerializer(data=py_date)
-        try:
-            serializer.is_valid()
-        except Exception as e:
-            return serializer.errors
-        project = Projects.objects.create(**serializer.validated_data)
-        return JsonResponse(serializer.data, status=201)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
